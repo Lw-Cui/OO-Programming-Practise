7d6a086856b0ac677f6abe0b6d993ef90afae730
Lw-Cui <645739460@QQ.com>
2015-09-17 21:58:12 +0800

    accomplish task v08

--- practice_v08/spec/main-spec.js
+++ practice_v08/spec/main-spec.js
@@ -1,6 +1,5 @@
 //  Your test here
-
-describe('tests of practice 01', function () {
+describe('tests of practice 07', function () {
 
     it("given Persion with age=21 and name is Tom, get introduce", function () {
 
@@ -8,6 +7,49 @@ describe('tests of practice 01', function () {
         var intro = person.introduce();
 
         expect(intro).toBe("My name is Tom. I am 21 years old.");
+    })
+
+
+    it("given Student with age=18 and name is lw, get introduce (about class)", function() {
+
+    	var my_class = new Class(2);
+    	var student = new Student(18, "lw", my_class);
+    	var intro = student.introduce();
+
+    	expect(intro).toBe("My name is lw. I am 18 years old. I am a Student. I am at Class 2."); 
+
+    	my_class.assginLeader(student);
+    	var intro = student.introduce();
+
+    	expect(intro).toBe("My name is lw. I am 18 years old. I am a Student. I am Leader of Class 2.");
+    })    
+
+
+    it("given teacher with age=20 and name is Jack, get introduce (Class 2 or No Class)", function() {
+
+    	var person = new Teacher(20, "Jack", 2);
+    	var intro = person.introduce();
+    	expect(intro).toBe("My name is Jack. I am 20 years old. I am a teacher. I teach Class 2.");
+
+    	var person = new Teacher(20, "Jack");
+    	var intro = person.introduce();
+    	expect(intro).toBe("My name is Jack. I am 20 years old. I am a teacher. I teach No Class.");
+    })
+    
+
+
+    it("given teacher with age=20 and name is Jack, get introduce", function() {
+
+    	var my_class = new Class(2);
+    	var student = new Student(18, "lw", my_class);
+
+    	var person = new Teacher(20, "Jack");
+    	var intro = person.introduceWith(student);
+    	expect(intro).toBe("My name is Jack. I am 20 years old. I am a teacher. I don't teach lw.");
+    	
+    	var person = new Teacher(20, "Jack", 2);
+    	var intro = person.introduceWith(student);
+    	expect(intro).toBe("My name is Jack. I am 20 years old. I am a teacher. I teach lw.");
     })
 
 });
--- practice_v08/src/main.js
+++ practice_v08/src/main.js
@@ -1,1 +1,61 @@
-//  Your code here
+var Person = function(age, name) {
+	this._age = age;
+	this._name = name;
+};
+
+Person.prototype.introduce = function() {
+	return "My name is " + this._name + ". I am " + this._age + " years old.";
+};
+
+
+var Class = function(number) {
+	this._number = number;
+	this.assginLeader = function(student) {
+		this._leader = student;
+	};
+};
+
+var Student = function(age, name, my_class) {
+	Person.call(this, age, name);
+	this._class = my_class;
+};
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+
+Student.prototype.introduce = function() {
+	var intro = Person.prototype.introduce.call(this) + " I am a Student."
+	if (this._class._leader == this)
+		intro += " I am Leader of Class " + this._class._number + "."
+	else
+		intro += " I am at Class " + this._class._number + ".";
+	return intro
+};
+
+
+var Teacher = function(age, name, my_class) {
+	Person.call(this, age, name);
+	this._class = my_class;
+};
+
+Teacher.prototype = Object.create(Person.prototype);
+Teacher.prototype.constructor = Teacher;
+
+
+Teacher.prototype.introduce = function() {
+	var intro = Person.prototype.introduce.call(this) + " I am a teacher."
+	if (this._class != undefined)
+		intro += " I teach Class " + this._class + ".";
+	else
+		intro += " I teach No Class.";
+	return intro;
+};
+
+Teacher.prototype.introduceWith = function(student) {
+	var intro = Person.prototype.introduce.call(this) + " I am a teacher."
+	if (this._class == student._class._number)
+		intro += " I teach " + student._name + ".";
+	else
+		intro += " I don't teach " + student._name + ".";
+	return intro;
+};
