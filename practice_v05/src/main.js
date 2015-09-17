313f16234843557eeef0d438ec722d7b13d3537c
Lw-Cui <645739460@QQ.com>
2015-09-17 15:46:23 +0800

    accomplish task v05

--- practice_v05/src/main.js
+++ practice_v05/src/main.js
@@ -1,1 +1,35 @@
 //  Your code here
+var Person = function(age, name) {
+	this._age = age;
+	this._name = name;
+};
+
+Person.prototype.introduce = function() {
+	return "My name is " + this._name + ". I am " + this._age + " years old.";
+};
+
+
+var Student = function(age, name, my_class) {
+	Person.call(this, age, name);
+	this._class = my_class;
+};
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+
+Student.prototype.introduce = function() {
+	return Person.prototype.introduce.call(this) + " I am a Student. I am at Class " + this._class + ".";
+};
+
+
+
+var Worker = function(age, name) {
+	Person.call(this, age, name);
+};
+
+Worker.prototype = Object.create(Person.prototype);
+Worker.prototype.constructor = Worker;
+
+Worker.prototype.introduce = function() {
+	return Person.prototype.introduce.call(this) + " I am a Worker. I have a job.";
+};
