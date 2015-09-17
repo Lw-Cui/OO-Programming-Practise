82b1cc395b38fda04c470b55b1cd8470359e094a
Lw-Cui <645739460@QQ.com>
2015-09-17 15:30:35 +0800

    accomplish task v04

--- practice_v04/spec/main-spec.js
+++ practice_v04/spec/main-spec.js
@@ -1,5 +1,4 @@
 //  Your test here
-
 describe('tests of practice 01', function () {
 
     it("given Persion with age=21 and name is Tom, get introduce", function () {
@@ -8,6 +7,22 @@ describe('tests of practice 01', function () {
         var intro = person.introduce();
 
         expect(intro).toBe("My name is Tom. I am 21 years old.");
+    })
+
+    it("given Student with age=18 and name is lw, get introduce", function() {
+
+    	var person = new Student(18, "lw", 2);
+    	var intro = person.introduce();
+
+    	expect(intro).toBe("My name is lw. I am 18 years old. I am a Student. I am at Class 2."); 
+    })
+
+    it("given worker with age=20 and name is Jack, get introduce", function() {
+
+    	var person = new Worker(20, "Jack");
+    	var intro = person.introduce();
+
+    	expect(intro).toBe("My name is Jack. I am 20 years old. I am a Worker. I have a job.");
     })
 
 });
--- practice_v04/src/main.js
+++ practice_v04/src/main.js
@@ -1,1 +1,40 @@
 //  Your code here
+var Person = function(age, name) {
+	this._age = age;
+	this._name = name;
+};
+
+Person.prototype.base_introduce = function() {
+	return "My name is " + this._name + ". I am " + this._age + " years old.";
+};
+
+Person.prototype.introduce = function() {
+	return this.base_introduce();
+}
+
+
+
+var Student = function(age, name, my_class) {
+	Person.call(this, age, name);
+	this._class = my_class;
+};
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+
+Student.prototype.introduce = function() {
+	return this.base_introduce() + " I am a Student. I am at Class " + this._class + ".";
+};
+
+
+
+var Worker = function(age, name) {
+	Person.call(this, age, name);
+};
+
+Worker.prototype = Object.create(Person.prototype);
+Worker.prototype.constructor = Worker;
+
+Worker.prototype.introduce = function() {
+	return this.base_introduce() + " I am a Worker. I have a job.";
+};
