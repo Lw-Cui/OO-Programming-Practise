fe8e470687f8bed19df3c6b28e0cff324786461f
Lw-Cui <645739460@QQ.com>
2015-09-17 15:19:57 +0800

    accomplish task v03

--- practice_v03/spec/main-spec.js
+++ practice_v03/spec/main-spec.js
@@ -10,4 +10,20 @@ describe('tests of practice 01', function () {
         expect(intro).toBe("My name is Tom. I am 21 years old.");
     })
 
+    it("given Student with age=18 and name is lw, get introduce", function() {
+
+    	var person = new Student(18, "lw", 2);
+    	var intro = person.introduce();
+
+    	expect(intro).toBe("I am a Student. I am at Class 2."); 
+    })
+
+    it("given worker with age=20 and name is Jack, get introduce", function() {
+
+    	var person = new Worker(20, "Jack");
+    	var intro = person.introduce();
+
+    	expect(intro).toBe("I am a Worker. I have a job.");
+    })
+
 });
