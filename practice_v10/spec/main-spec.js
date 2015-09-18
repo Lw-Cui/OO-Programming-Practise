f4d1a4392f0e2ef0ac7fce64ca1e200b8b67a369
Lw-Cui <645739460@QQ.com>
2015-09-18 21:01:42 +0800

    accomplish task v10

--- practice_v10/spec/main-spec.js
+++ practice_v10/spec/main-spec.js
@@ -1,6 +1,5 @@
 //  Your test here
-
-describe('tests of practice 01', function () {
+describe('tests of practice 07', function () {
 
     it("given Persion with age=21 and name is Tom, get introduce", function () {
 
@@ -8,6 +7,65 @@ describe('tests of practice 01', function () {
         var intro = person.introduce();
 
         expect(intro).toBe("My name is Tom. I am 21 years old.");
+    })
+
+
+    it("given Student with age=18 and name is lw, get introduce (about class)", function() {
+
+    	var my_class = new Class(3);
+    	var student = new Student(18, "lw", my_class);
+    	my_class.appendMember(student);
+    	var intro = student.introduce();
+
+    	expect(intro).toBe("My name is lw. I am 18 years old. I am a Student. I am at Class 3."); 
+
+    	my_class.assginLeader(student);
+    	var intro = student.introduce();
+
+    	expect(intro).toBe("My name is lw. I am 18 years old. I am a Student. I am Leader of Class 3.");
+
+    	var your_class = new Class(2);
+    	var new_student = new Student(18, "lw", your_class);
+    	my_class.assginLeader(new_student);
+    	var intro = new_student.introduce();
+
+    	expect(intro).toBe("My name is lw. I am 18 years old. I am a Student. I am at Class 2."); 
+    })    
+
+
+    it("given teacher with age=20 and name is Jack, get introduce (Class 2 or No Class)", function() {
+
+    	var my_class_1 = new Class(2);
+    	var my_class_2 = new Class(3);
+
+    	var person = new Teacher(20, "Jack", [my_class_1, my_class_2]);
+    	var intro = person.introduce();
+    	expect(intro).toBe("My name is Jack. I am 20 years old. I am a teacher. I teach Class 2, 3.");
+
+    	var person = new Teacher(20, "Jack");
+    	var intro = person.introduce();
+    	expect(intro).toBe("My name is Jack. I am 20 years old. I am a teacher. I teach No Class.");
+    })
+    
+
+
+    it("given teacher with age=20 and name is Jack, get introduce", function() {
+
+    	var my_class = new Class(2);
+    	var your_class = new Class(3);
+    	var student = new Student(18, "lw", my_class);
+
+    	var person = new Teacher(20, "Jack", [my_class, your_class]);
+    	var intro = person.introduceWith(student);
+    	expect(intro).toBe("My name is Jack. I am 20 years old. I am a teacher. I teach lw.");
+    	
+    	var person = new Teacher(20, "Jack");
+    	var intro = person.introduceWith(student);
+    	expect(intro).toBe("My name is Jack. I am 20 years old. I am a teacher. I don't teach lw.");
+
+    	var person = new Teacher(20, "Jack", [your_class]);
+    	var intro = person.introduceWith(student);
+    	expect(intro).toBe("My name is Jack. I am 20 years old. I am a teacher. I don't teach lw.");
     })
 
 });
--- practice_v10/src/main.js
+++ practice_v10/src/main.js
@@ -1,1 +1,88 @@
 //  Your code here
+var Person = function(age, name) {
+	this._age = age;
+	this._name = name;
+};
+
+Person.prototype.introduce = function() {
+	return "My name is " + this._name + ". I am " + this._age + " years old.";
+};
+
+
+var Class = function(number) {
+	this._number = number;
+	this._student = [];
+	this.assginLeader = function(student) {
+		if (student._class == this)
+			this._leader = student;
+		else
+			console.log("It is not one of us.");
+	};
+	this.appendMember = function(student) {
+		this._student.push(student);
+	};
+};
+
+Class.prototype.isIn = function(student) {
+	if (student._class == this)
+		return true;
+	else
+		return false;
+};
+
+var Student = function(age, name, my_class) {
+	Person.call(this, age, name);
+	this._class = my_class;
+	my_class.appendMember(this);
+};
+
+Student.prototype = Object.create(Person.prototype);
+Student.prototype.constructor = Student;
+
+Student.prototype.introduce = function() {
+	var intro = Person.prototype.introduce.call(this) + " I am a Student."
+	if (this._class._leader == this)
+		intro += " I am Leader of Class " + this._class._number + ".";
+	else
+		intro += " I am at Class " + this._class._number + ".";
+	return intro
+};
+
+
+var Teacher = function(age, name, my_classes) {
+	Person.call(this, age, name);
+	this._classes = my_classes;
+};
+
+Teacher.prototype = Object.create(Person.prototype);
+Teacher.prototype.constructor = Teacher;
+
+
+Teacher.prototype.introduce = function() {
+	var intro = Person.prototype.introduce.call(this) + " I am a teacher."
+	if (this._classes != undefined && this._classes.length) {
+		intro += " I teach Class";
+		for (var i in this._classes)
+			intro += " " + this._classes[i]._number + ",";
+		intro = intro.substr(0, intro.length - 1) + '.';
+	} else {
+		intro += " I teach No Class.";
+	}
+	return intro;
+};
+
+Teacher.prototype.introduceWith = function(student) {
+	var intro = Person.prototype.introduce.call(this) + " I am a teacher."
+	if (this.isTeaching(student))
+		intro += " I teach " + student._name + ".";
+	else
+		intro += " I don't teach " + student._name + ".";
+	return intro;
+};
+
+Teacher.prototype.isTeaching = function(student) {
+	for (var i in this._classes)
+		if (this._classes[i].isIn(student))
+			return true;
+	return false;
+};
